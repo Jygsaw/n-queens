ec39042a5d31d85c0ef95746285d80f82517f09c
Hack Reactor Students <jygsaw@gmail.com>
2014-06-11 16:09:04 -0700

    Completed find NRooks & countNRooks Solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,7 +14,17 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var rows = [];
+  for (var i = 0; i < n; i++) {
+    rows[i] = 0;
+  }
+  for (var i = 0; i < n; i++) {
+    solution[i] = rows.concat();
+  }
+  for (var i = 0; i < n; i++) {
+    solution[i][i] = 1;
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -24,7 +34,10 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 1;
+  for (var i = 1; i <= n; i++) {
+    solutionCount *= i;
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
