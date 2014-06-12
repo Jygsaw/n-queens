cdd8dc7e61fe13f2cb4c3f6b34cd3884e790c2e1
Hack Reactor Students <jygsaw@gmail.com>
2014-06-12 10:41:25 -0700

    Finished N-QueensCount under 3 seconds

--- src/solvers.js
+++ src/solvers.js
@@ -19,7 +19,7 @@ window.findNRooksSolution = function(n) {
     solution[i][i] = 1;
   }
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
@@ -32,7 +32,7 @@ window.countNRooksSolutions = function(n) {
     solutionCount *= i;
   }
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
@@ -42,13 +42,14 @@ window.countNRooksSolutions = function(n) {
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  // console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
+  console.log(">>>>> COUNTING *" + n + "*QUEENS <<<<<");
   var solutionCount = 0;
 
   var placeNewQueen = function(row, ld, col, rd) {
@@ -60,62 +61,83 @@ console.log("rd: " + rd);
 console.log("col: ", col);
 
     // calculate poss
-    var pos = '';
+    var poss = '';
     for (var i = 0; i < col.length; i++) {
       if (row > 1) {
         if (col[i] === '1' || ld[i] === '1' || rd[i] === '1') {
-          pos += '0';
+          poss += '0';
         }
         else {
-          pos += '1';
+          poss += '1';
         }
       }
       else {
-        pos += "0";
+        poss += "0";
       }
     }
+    // TODO remove debugging
+    console.log("poss:", poss);
 
-    console.log("pos:", pos);
-          // // check poss for next level
-          // if (new poss != crap) {
-          //    recurse with ld, col, rd, new poss
-          // }
-
+    // convert poss into number
+    // if poss === 0, then no more moves
+    var possNum = parseInt(poss, 2);
+    if (possNum === 0 && row !== 1) {
+      return;
+    }
     if (n === row) {
       solutionCount++;
+      return;
     } else {
       // iterate through good poss
+      console.log("Here", poss);
       for (var i = 0; i < poss.length; i++) {
-        if (poss[i] === '0') {
+        if (poss[i] === '1' || row === 1 && poss[i] === '0') {
+console.log(">> Placing Queen at poss i: " + i);
           // create column mask
+console.log("===== COLUMN CHECK ======")
           var newCol = col.split('');
+console.log("= pre: " + newCol);
           newCol[i] = 1;
-          newCol = newCol.join();
-
-          // create ld mask
+          newCol = newCol.join('');
+console.log("= post: " + newCol);
+          // create ld mask by shifting bits
           var newLd = ld.split('');
           newLd[i] = 1;
+console.log("=== shifting LD ===");
+console.log("= pre: " + newLd);
+          newLd.shift();
+          newLd.push('0');
+          newLd = newLd.join('');
+console.log("= post: " + newLd);
 
-          newLd = newLd.join();
-
-
+          // create rd mask by shifting bits
           var newRd = rd.split('');
           newRd[i] = 1;
-
-          newRd = newRd.join();
-
-          // shift diagonal masks
+console.log("=== shifting RD ===");
+console.log("= pre: " + newRd);
+          newRd.unshift(0);
+          newRd.pop();
+          newRd = newRd.join('');
+console.log("= pre: " + newRd);
 
           // recurse with new masks
-//          placeNewQueen(row + 1, newCol, newLd, newRd);
+         placeNewQueen(row + 1, newLd, newCol, newRd);
         }
       }
     }
   };
 
-  var baseMask = "00000000";
-  placeNewQueen(1, baseMask, baseMask, baseMask);
+  var baseMask = '';
+  for (var i = 0; i < n; i++) {
+    baseMask += "0";
+  }
 
+  if (n > 0) {
+    placeNewQueen(1, baseMask, baseMask, baseMask);
+  }
+  else {
+    solutionCount++;
+  }
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
 
   return solutionCount;
